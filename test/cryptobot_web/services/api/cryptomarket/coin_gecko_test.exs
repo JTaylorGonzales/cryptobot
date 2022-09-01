b278fae93f2b51bcc1a617fda2eac411abb467ca
jtaylorgonzales <joshua@mashupgarage.com>
2022-09-01 10:03:42 +0800

    adds test for searching coins

--- test/cryptobot_web/services/api/cryptomarket/coin_gecko_test.exs
+++ test/cryptobot_web/services/api/cryptomarket/coin_gecko_test.exs
@@ -19,4 +19,23 @@ defmodule CryptobotWeb.Services.Api.CryptoMarket.CoinGeckoTest do
       assert {:error, :coin_gecko_api_error} = CoinGecko.get_coin_data("errorMock")
     end
   end
+
+  describe "search_coin/2" do
+    test "it should return the formmated search result" do
+      {:ok, %{"bitcoin" => bitcoin, "huobi-btc" => _hbtc, "wrapped-bitcoin" => _wbtc}} =
+        CoinGecko.search_coin("bitcoin", 5)
+
+      assert %{name: "Bitcoin", thumb: thumb} = bitcoin
+      assert is_binary(thumb)
+    end
+
+    test "it should limit the results" do
+      {:ok, data} = CoinGecko.search_coin("bitcoin", 2)
+      assert Enum.count(data) == 2
+    end
+
+    test "it should return an error when something went wrong with the api" do
+      assert {:error, :coin_gecko_api_error} = CoinGecko.search_coin("errorMock", 5)
+    end
+  end
 end
