4531a0a3b4559e6d6142d4808cb126d48e22d0ce
jtaylorgonzales <joshua@mashupgarage.com>
2022-09-01 14:40:17 +0800

    adds automatic encoding of request body

--- lib/cryptobot_web/services/api/facebook_api.ex
+++ lib/cryptobot_web/services/api/facebook_api.ex
@@ -1,9 +1,11 @@
 defmodule CryptobotWeb.Services.Api.FacebookApi do
   use HTTPoison.Base
 
-  @url "https://graph.facebook.com/v14.0/me/messages?"
+  @url "https://graph.facebook.com/v14.0/me/"
 
   def process_url(url), do: @url <> url
+
+  def process_request_body(body), do: Jason.encode!(body)
 
   def process_response_body(body), do: Jason.decode!(body)
 end
