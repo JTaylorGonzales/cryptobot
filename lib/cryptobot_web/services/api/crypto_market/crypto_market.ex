efcbae3a0bbba546c115dbf6439a813fdacf90cb
jtaylorgonzales <joshua@mashupgarage.com>
2022-09-01 10:03:19 +0800

    adds search coin

--- lib/cryptobot_web/services/api/crypto_market/crypto_market.ex
+++ lib/cryptobot_web/services/api/crypto_market/crypto_market.ex
@@ -1,13 +1,20 @@
 defmodule CryptobotWeb.Services.Api.CryptoMarket do
   alias CryptobotWeb.Services.Api.CryptoMarket.CoinGecko
 
-  @valid_markets ["coin_gecko", "mock"]
+  @valid_markets [:coin_gecko]
 
-  def get_coin_data(identifier, market \\ "coin_gecko") when market in @valid_markets do
+  def get_coin_data(identifier, market \\ :coin_gecko) when market in @valid_markets do
     assign_market(market).get_coin_data(identifier)
   end
 
+  def get_coin_data(_id, _market), do: {:error, :invalid_market}
+
+  def search_coin(name, limit \\ 5, market \\ :coin_gecko) when market in @valid_markets do
+    assign_market(market).search_coin(name, limit)
+  end
+
+  def search_coin(_name, _limit, _market), do: {:error, :invalid_market}
   # this will allow us to easily implement other 3rd party crypto market API's
   # but we need to make sure that they will implement the same behaviour
-  def assign_market("coin_gecko"), do: CoinGecko
+  def assign_market(:coin_gecko), do: CoinGecko
 end
