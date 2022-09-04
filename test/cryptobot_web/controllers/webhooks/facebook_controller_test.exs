f95f2d7b6f1e4aac2a8f53d37afabb10e03933f3
jtaylorgonzales <joshua@mashupgarage.com>
2022-09-05 03:34:35 +0800

    adds facebook hub challenge

---
+++ test/cryptobot_web/controllers/webhooks/facebook_controller_test.exs
@@ -0,0 +1,19 @@
+defmodule CryptobotWeb.Webhooks.FacebookControllerTest do
+  use CryptobotWeb.ConnCase
+
+  describe "GET handle_event/2" do
+    test "should verify verify the hub challenge", %{conn: conn} do
+      conn =
+        get(
+          conn,
+          Routes.facebook_path(conn, :handle_event, %{
+            "hub.challenge" => "test_challenge",
+            "mode" => "subscribe",
+            "token" => Application.get_env(:cryptobot, :facebook)[:verification_token]
+          })
+        )
+
+      assert %{status: 200, resp_body: "test_challenge"} = conn
+    end
+  end
+end
