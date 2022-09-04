560019a07a09081c29d654ddd9ff7a1642ca5aab
jtaylorgonzales <joshua@mashupgarage.com>
2022-09-05 03:34:22 +0800

    adds logger

--- lib/cryptobot_web/services/api/facebook/facebook.ex
+++ lib/cryptobot_web/services/api/facebook/facebook.ex
@@ -1,40 +1,25 @@
 defmodule CryptobotWeb.Services.Api.Facebook do
-  @api CryptobotWeb.Services.Api.FacebookApi
-  @token "EAAFIKUmZBM3wBAJah1CFSYLbau0zTqzDbaQACT2ZAd2fnxIq4H1ohruB7BxRrBXr5972epursBKdpnwgcTyTFuSrxOaweHCejQnxoyodUUO7DYCvEJNyuJKA342PcmWaJ6uSBFzeEXVCg4p9PMQiZA8b2uOTVSqmw1RFSw8D8YsklqaoHZARo1U9ua5qVS1ZBYcGmBkZAenwZDZD"
-
-  def set_greeting do
-    body =
-      Jason.encode!(%{
-        greeting: [
-          %{
-            locale: "default",
-            text: "Hello {{user_first_name}}! welcome to CryptoBot the best crypto bot"
-          }
-        ]
-      })
-
-    @api.post("messenger_profile?access_token=#{@token}", body, [
-      {"Content-Type", "application/json"}
-    ])
-  end
-
-  def set_get_started do
-    body =
-      Jason.encode!(%{
-        get_started: %{
-          payload: "GET_STARTED"
-        }
-      })
-
-    @api.post("messenger_profile?access_token=#{@token}", body, [
-      {"Content-Type", "application/json"}
-    ])
-  end
+  require Logger
+  @api Application.get_env(:cryptobot, :facebook)[:facebook_api]
+  @token Application.get_env(:cryptobot, :facebook)[:access_token]
 
   def send_message(body) do
-    @api.post("messages?access_token=#{@token}", body, [
-      {"Content-Type", "application/json"}
-    ])
-    |> IO.inspect()
+    case @api.post("messages?access_token=#{@token}", body) do
+      {:ok, %{status_code: 200}} ->
+        {:ok, :success}
+
+      {:ok, %{status_code: 401}} ->
+        Logger.critical("Refresh token needs to be refreshed")
+
+      {:ok, %{status_code: status_code, body: body}} ->
+        response = Jason.encode!(body)
+
+        Logger.error(
+          "Failed to send a response. status_code: #{status_code}, response: #{response} "
+        )
+
+      {:error, error} ->
+        Logger.error("Unexpected error: #{error}")
+    end
   end
 end
